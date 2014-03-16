4ccb59b0873517f5fb3e2cfd34c0b83ceb5de4be
marcroche <marcroche@triad.rr.com>
2014-03-16 15:09:52 -0400

    update js paths

--- src/app/main.js
+++ src/app/main.js
@@ -1,10 +1,10 @@
 requirejs.config(
     {
         paths: {
-            'jQuery': '../js/jquery/dist/jquery',
-            'underscore': '../js/underscore/underscore',
-			'd3' : '../js/d3/d3',
-            "Q": "../js/q/q",
+            'jQuery': '../js/jquery',
+            'underscore': '../js/underscore',
+			'd3' : '../js/d3',
+            "Q": "../js/q",
         },
         shim: {
             'jQuery': {
