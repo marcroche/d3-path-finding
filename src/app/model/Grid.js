26c319fb453fcea3e7a96ec1e8e8f1d3d2d2036c
marcroche <marcroche@triad.rr.com>
2014-05-26 18:02:46 -0400

    change module structure

--- src/app/model/Grid.js
+++ src/app/model/Grid.js
@@ -1,55 +1,47 @@
 define(['model/Vertex', 'model/Edge'], function(Vertex, Edge) {
 
-    var vertices = [];
-    var self;
-    var cols;
-    var rows;
-    
-    function findNeighbors() {
-        for(var r = 0; r < rows; r++) {
-            for(var c = 0; c < cols; c++) {
-                if(c - 1 >= 0) {
-                    vertices[r][c].edges.push(new Edge(vertices[r][c], vertices[r][c - 1], 1.0));
-                }
-
-                if(c + 1 < cols) {
-                    vertices[r][c].edges.push(new Edge(vertices[r][c], vertices[r][c + 1], 1.0));
-                }
-
-                if(r - 1 >= 0) {
-                    vertices[r][c].edges.push(new Edge(vertices[r][c], vertices[r - 1][c], 1.0));
-                }
-
-                if(r + 1 < rows) {
-                    vertices[r][c].edges.push(new Edge(vertices[r][c], vertices[r + 1][c], 1.0));
-                }
-            }
-        }
-    }
-    
-    function createGraph(_rows, _cols) {
-        vertices = [];
-        for(var i = 0; i < rows; i++) {
-            var r = [];
-            for (var j = 0; j < cols; j++) {
-                r.push(new Vertex(i, j));
-            }
-            vertices.push(r);
-        }
-    }
-
-    var api = function(_rows, _cols) {
-        self = this;
-        rows = _rows;
-        cols = _cols;
-
-        createGraph(rows, cols);
-        findNeighbors();
-
-        this.vertices = vertices;
-        this.rows = rows;
+    var Grid = function(rows, cols) {
+        var self = this;
+        this.vertices = [];
         this.cols = cols;
+        this.rows = rows;
+
+        function findNeighbors() {
+            for(var r = 0; r < self.rows; r++) {
+                for(var c = 0; c < self.cols; c++) {
+                    if(c - 1 >= 0) {
+                        self.vertices[r][c].edges.push(new Edge(self.vertices[r][c], self.vertices[r][c - 1], 1.0));
+                    }
+
+                    if(c + 1 < cols) {
+                        self.vertices[r][c].edges.push(new Edge(self.vertices[r][c], self.vertices[r][c + 1], 1.0));
+                    }
+
+                    if(r - 1 >= 0) {
+                        self.vertices[r][c].edges.push(new Edge(self.vertices[r][c], self.vertices[r - 1][c], 1.0));
+                    }
+
+                    if(r + 1 < rows) {
+                        self.vertices[r][c].edges.push(new Edge(self.vertices[r][c], self.vertices[r + 1][c], 1.0));
+                    }
+                }
+            }
+        }
+        
+        function createGraph() { 
+            self.vertices = [];
+            for(var i = 0; i < self.rows; i++) {
+                var r = [];
+                for (var j = 0; j < self.cols; j++) {
+                    r.push(new Vertex(i, j));
+                }
+                self.vertices.push(r);
+            }
+        }
+
+        createGraph();
+        findNeighbors();
     };
 
-    return api;
+    return Grid;
 });
