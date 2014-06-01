1149a3e44fc9036a6b9c8119e32854b60967f725
marcroche <marcroche@triad.rr.com>
2014-06-01 14:27:35 -0400

    Refactor Dijkstra design

--- src/app/finders/Dijkstra.js
+++ src/app/finders/Dijkstra.js
@@ -1,97 +1,96 @@
 define(['Q', 'model/MinPriorityQueue'], function(Q, MinPriorityQueue) {
-    var priorityQueue;
-    var distTo = [];
-    var shortestEdges = [];
-    var startVertex, endVertex;
-    
-    function relax(edge) {
-        var source = edge.source;
-        var target = edge.target;
 
-        if (target.isBlocked) {
-            return;
-        }
+    var Dijkstra = function() {
+        var priorityQueue = new MinPriorityQueue('key', 'cost', []);
+        var distTo = [];
+        var shortestEdges = [];
+        var startVertex, endVertex;
 
-        if (target.cost > source.cost + edge.weight) {
-            target.cost = source.cost + edge.weight;
+        function relax(edge) {
+            var source = edge.source;
+            var target = edge.target;
 
-            var se = _.findWhere(shortestEdges, { key: target.key });
-            if (se !== undefined) {
-                se.edge = edge;
-            } else {
-                shortestEdges.push({ key: target.key, edge: edge });
+            if (target.isBlocked) {
+                return;
             }
 
-            if (priorityQueue.contains(target.key)) {
-                priorityQueue.decreaseKey(target.heapIndex, target.cost);
-            } else {
-                priorityQueue.push(target);
+            if (target.cost > source.cost + edge.weight) {
+                target.cost = source.cost + edge.weight;
+
+                var se = _.findWhere(shortestEdges, { key: target.key });
+                if (se !== undefined) {
+                    se.edge = edge;
+                } else {
+                    shortestEdges.push({ key: target.key, edge: edge });
+                }
+
+                if (priorityQueue.contains(target.key)) {
+                    priorityQueue.decreaseKey(target.heapIndex, target.cost);
+                } else {
+                    priorityQueue.push(target);
+                }
             }
         }
-    }
 
-    function shortestPath(destinationKey) {
-        var path = [];
-        var e = _.findWhere(shortestEdges, { key: destinationKey });
+        function shortestPath(destinationKey) {
+            var path = [];
+            var e = _.findWhere(shortestEdges, { key: destinationKey });
 
-        if (e === undefined) {
+            if (e === undefined) {
+                return path;
+            }
+
+            while (e !== undefined) {
+                e = _.findWhere(shortestEdges, { key: e.edge.source.key });
+                path.unshift(e);
+            }
             return path;
         }
 
-        while (e !== undefined) {
-            e = _.findWhere(shortestEdges, { key: e.edge.source.key });
-            path.unshift(e);
-        }
-        return path;
-    }
+        function search(_startVertex, _endVertex) {
+            var deferred = Q.defer();
+            
+            startVertex = _startVertex;
+            endVertex = _endVertex;
 
-    function search(_startVertex, _endVertex) {
-        var deferred = Q.defer();
-        
-        startVertex = _startVertex;
-        endVertex = _endVertex;
+            priorityQueue.push(startVertex);
+            shortestEdges = [];
 
-        priorityQueue.push(startVertex);
-        shortestEdges = [];
+            var runner = setInterval(function () {
+                var v = priorityQueue.pop();
+                v.heapIndex = -1;
+                for (var i = 0; i < v.edges.length; i++) {
+                    relax(v.edges[i]);
+                }
+                v.visited = true;
+                deferred.notify({
+                    status: 'visited'
+                });
 
-        var runner = setInterval(function () {
-            var v = priorityQueue.pop();
-            v.heapIndex = -1;
-            for (var i = 0; i < v.edges.length; i++) {
-                relax(v.edges[i]);
-            }
-            v.visited = true;
-            deferred.notify({
-                status: 'visited'
-            });
+                if (priorityQueue.isEmpty()) {
+                    clearInterval(runner);
+                    var sp = shortestPath(endVertex.key);
 
-            if (priorityQueue.isEmpty()) {
-                clearInterval(runner);
-                var sp = shortestPath(endVertex.key);
-
-                if (sp.length === 0) {
+                    if (sp.length === 0) {
+                        deferred.resolve();
+                    }
+                    for (var s = 0; s < sp.length; s++) {
+                        if (sp[s] !== undefined) {
+                            sp[s].edge.target.isOnPath = true;
+                        }
+                    }
+                    deferred.notify({
+                        status: 'complete'
+                    });
                     deferred.resolve();
                 }
-                for (var s = 0; s < sp.length; s++) {
-                    if (sp[s] !== undefined) {
-                        sp[s].edge.target.isOnPath = true;
-                    }
-                }
-                deferred.notify({
-                    status: 'complete'
-                });
-                deferred.resolve();
-            }
-        }, 25);
+            }, 25);
 
-        return deferred.promise;
-    }
-
-    var api = function () {
-        priorityQueue = new MinPriorityQueue('key', 'cost', []);
+            return deferred.promise;
+        }
 
         this.search = search;
     };
 
-    return api;
+    return Dijkstra;
 });
